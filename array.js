7fa401f137c8e833f85da40b55a080cc48e04758
Christian Mina <christian.mina@tup.edu.ph>
2022-03-17 00:24:39 +0800

    Fix linting issue

--- array.js
+++ array.js
@@ -4,9 +4,9 @@ let elements = [0.9, 2, 3, 4, 5, 6, 7, 8, 9];
 //Assign smallest to iterate elements.
 let smallest = elements[0];
 
-//Use for loop to discover the lenght of the array.
+//Use for loop to discover the length of the array.
 for (let i = 1; i < elements.length; i++) {
-    //Set up elements to discover the smallest number.
+  //Set up elements to discover the smallest number.
   if (elements[i] < smallest) {
     smallest = arr[i];
   }
