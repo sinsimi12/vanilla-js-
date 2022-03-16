9cdff7f4c7ed17facc4ce57684ae72378d31324d
Christian Mina <christian.mina@tup.edu.ph>
2022-03-16 23:57:59 +0800

    Add for loop and given elements.

---
+++ array.js
@@ -0,0 +1,16 @@
+//Given Elements.
+let elements = [0.9, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//Assign smallest to iterate elements.
+let smallest = elements[0];
+
+//Use for loop to discover the lenght of the array.
+for (let i = 1; i < elements.length; i++) {
+    //Set up elements to discover the smallest number.
+  if (elements[i] < smallest) {
+    smallest = arr[i];
+  }
+}
+
+//Return the smallest element
+console.log("The smallest element is " + smallest);
